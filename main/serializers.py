a864c216d5b8182ce077f3699cb0c8b45a145253
Dizlat <dizlat.ur@gmail.com>
2021-06-21 17:17:29 +0600

    serialize

---
+++ main/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    pass
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    pass
+
+
+class OrderItemsSerializer(serializers.ModelSerializer):
+    pass
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    pass
