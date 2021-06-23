c2919670d364cbe28a146ebd9e2625765fe455b9
Dizlat <dizlat.ur@gmail.com>
2021-06-23 17:07:44 +0600

    serializer order

--- main/serializers.py
+++ main/serializers.py
@@ -43,8 +43,29 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class OrderItemsSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = OrderItems
+        fields = ('product', 'quantity')
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    pass
+    products = OrderItemsSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ('products', 'notes')
+
+    def create(self, validated_data):
+        total_sum = 0
+        request = self.context.get('request')
+        validated_data['user'] = request.user
+        validated_data['status'] = 'new'
+        validated_data['total_sum'] = total_sum
+        products = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+        for prod in products:
+            total_sum += prod['product'].price * prod['quantity']
+            OrderItems.objects.create(order=order, **prod)
+        order.total_sum = total_sum
+        order.save()
+        return order
