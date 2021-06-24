5eb099ff4e9a76e50f53273bbe1a513255de9b5f
Dizlat <dizlat.ur@gmail.com>
2021-06-24 16:34:19 +0600

    register

--- account/serializers.py
+++ account/serializers.py
@@ -1,8 +1,54 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+User = get_user_model()
 
-class RegisterSerializer(serializers.Serializer):
-    pass
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=6, required=True)
+    password_confirm = serializers.CharField(min_length=6, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password_confirm', 'first_name', 'last_name']
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Такой пользователь уже зарегистрирован')
+        return email
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirm = attrs.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.create_activation_code()
+        user.send_activation_email(user.email, user.activation_code)
+        return user
+
+
+class ActivationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    activation_code = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        activation_code = attrs.get('activation_code')
+        if not User.objects.filter(email=email, activation_code=activation_code).exists():
+            raise serializers.ValidationError('Невернные введенные данные')
+        return attrs
+
+    def activate(self):
+        data = self.validated_data
+        user = User.objects.get(**data)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+
 
 
 class LoginSerializer(serializers.Serializer):
--- account/views.py
+++ account/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 
-"""
-1. Регистрация
-2. Активация
-3. login
-4. Востановление пароля
-5. Смена пароля 
-6, Профиль пользователя
-"""
+# 1. Регистрация
+# 2. Активация
+# 3. login
+# 4. Востановление пароля
+# 5. Смена пароля
+# 6, Профиль пользователя
+
+
+class RegisterView(APIView):
+    pass
+
+
+class ActivationView(APIView):
+    pass
+
+
+class LoginView():
+    pass
+
+
+class LogoutView(APIView):
+    pass
+
+
+class ResetPasswordView(APIView):
+    pass
+
+
+class ChangePasswordView(APIView):
+    pass
+
+
+class UserProfileView(APIView):
+    pass
+
